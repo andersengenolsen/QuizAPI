10c9517d9c4e1a0f4cf14c6fc59fd972bf4d2746
andersengenolsen <anderseols@gmail.com>
2018-10-02 14:56:36 +0200

    Added in-memory authentication, todo: jdbc auth

---
+++ src/main/java/springquiz/config/AppSecurityConfig.java
@@ -0,0 +1,42 @@
+package springquiz.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+
+@Configuration
+@EnableWebSecurity
+public class AppSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        User.UserBuilder users = User.withDefaultPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser(users.username("anders").password("test123").roles("admin"))
+                .withUser(users.username("anders2").password("test123").roles("user"));
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/api/questions/**").hasAnyRole("admin", "user")
+                .antMatchers(HttpMethod.POST, "/api/questions/").hasRole("admin")
+                .antMatchers(HttpMethod.PUT, "/api/questions/").hasRole("admin")
+                .antMatchers(HttpMethod.DELETE, "/api/questions/**").hasRole("admin")
+                .and()
+                .httpBasic()
+                .and()
+                .csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+
+}
