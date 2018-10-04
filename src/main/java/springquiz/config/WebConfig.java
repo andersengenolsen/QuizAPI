29e8bf9dd198a0e6f9d8f61c860b01a6fb4dac51
andersengenolsen <anderseols@gmail.com>
2018-10-04 17:37:29 +0200

    CORS requests allowed

--- src/main/java/springquiz/config/WebConfig.java
+++ src/main/java/springquiz/config/WebConfig.java
@@ -2,7 +2,9 @@ package springquiz.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
  * Configuration class for the API.
@@ -10,6 +12,11 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "springquiz.*")
-public class WebConfig {
+public class WebConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/questions/**").allowedMethods("PUT", "GET", "DELETE", "POST");
+    }
 }
 
